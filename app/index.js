3fdc9167d5f8f682ca3a06d40f8bc3e51bc2cd47
root <root@ip-172-31-27-12.ec2.internal>
2025-09-27 05:32:05 +0000

    jhaebka

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Todo App!\n');
+  res.end('Hello from Todo App-version-1!\n');
 });
 
 server.listen(PORT, () => {
